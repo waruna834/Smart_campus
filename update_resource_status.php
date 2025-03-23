6872c0a51f824dbb90e51acbe61a5a914b06fa4a
Weemandra Munasinghe <munasinghej00@gmail.com>
2025-03-23 18:08:01 +0530

    Update update_resource_status.php

--- update_resource_status.php
+++ update_resource_status.php
@@ -1,3 +1,13 @@
+//Developed by group leader Waruna Munasinghe (K2463495)
+
+//This PHP script is designed to update the availability status of resources in a MySQL database, specifically for admin users. 
+It begins by checking if the user is logged in and has the role of "admin"; 
+if not, it redirects them to the login page. Upon receiving a POST request, 
+the script retrieves the resource ID and the new status from the submitted data, 
+determining whether the status should be set to "Available" or "Unavailable." 
+It then prepares and executes an SQL UPDATE statement to change the status of the specified resource in the resource_availability table. 
+Finally, the script returns a success or error message based on the outcome of the update operation and closes the database connection.
+
 <?php
 //for resource availablity form (for admin only)
 session_start();

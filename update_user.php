8703960073b43be3ed474451842ec321a42893d9
Weemandra Munasinghe <munasinghej00@gmail.com>
2025-03-23 17:47:04 +0530

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -1,3 +1,14 @@
+//Developed by group member Tharushi Dissanayake (K2462662)
+
+//This PHP script allows a super admin to update the email and username of registered users in a MySQL database. 
+It starts by checking if the user is logged in and has the role of super admin; 
+if not, it redirects them to the login page. Upon receiving a POST request, 
+the script checks for the presence of a user ID and then processes updates for either the email or 
+username based on the submitted data. It uses prepared statements to securely execute the SQL update queries, 
+ensuring that user input is properly handled to prevent SQL injection. 
+Finally, the script returns a success message if the update is successful or an error message if it fails, 
+and it closes the database connection afterward.
+
 <?php
 //for registered users email and user name changes (only super admin can accessed)
 session_start();

e1985e06541bee994fb8a3e415cdc1bf85173779
Weemandra Munasinghe <munasinghej00@gmail.com>
2025-03-23 18:09:04 +0530

    Update update_status.php

--- update_status.php
+++ update_status.php
@@ -1,3 +1,12 @@
+//Developed by group leader Waruna Munasinghe (K2463495)
+
+//This PHP script is designed to update the status of a program schedule entry in a MySQL database, specifically for admin users. 
+It starts by checking if the user is logged in and has the role of "admin"; if not, 
+it redirects them to the login page. When a POST request is received containing an ID and a new status, 
+the script prepares an SQL UPDATE statement to change the status of the specified program schedule entry in the program_schedule table. 
+It uses a prepared statement to safely execute the query, allowing for the possibility of empty status values. 
+Finally, the script provides feedback on whether the status update was successful or if an error occurred, and it closes the database connection afterward.
+
 <?php
 //for program sheduling (admin view program schedule - status change)
 session_start();
